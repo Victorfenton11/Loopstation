0fc0ad07cddc5dd0160e15e2e451efa4110e7488
Victorfenton11 <victorfenton.a@hotmail.com>
2024-12-15 18:59:53 -0500

    Add python file

---
+++ loopstation.py
@@ -0,0 +1,15 @@
+# 12/15/2024 - Victor Fenton Aguilar
+# This program will create a graphical application that simulates the
+# functionality of a simple loopstation. Inspired by the BOSS 505 Loopstation.
+
+import tkinter as tk
+
+# This function will create the main application interface using Tkinter
+def build():
+    # TODO
+    pass
+
+def main():
+    build()
+
+main()
